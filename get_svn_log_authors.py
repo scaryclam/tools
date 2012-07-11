2d6290214f73e25ea7f968244768346c1747ce65
Becky Lewis <becky.lewis@tangentlabs.co.uk>
2012-07-11 16:08:31 +0100

    Added some command line options

--- get_svn_log_authors.py
+++ get_svn_log_authors.py
@@ -1,4 +1,7 @@
 import os
+import sys
+#import argparse
+import optparse
 
 
 def get_authors(filename):
@@ -20,16 +23,38 @@ def get_authors(filename):
     return authors
 
 
-if __name__ == '__main__':
+def write_output(filename, authors, email_host=None):
+    if not email_host:
+        email_host = "example.com"
+
     # Add the input file here. Command line options to follow
-    input_file = ''
-    authors = get_authors(input_file)
     default_line = "%(author)s = %(author_name)s <%(author_email)s>\n"
-    with open("authors.txt", 'w') as authors_fd:
+
+    with open(filename, 'w') as authors_fd:
         for author in authors:
             author_name = author
-            author_email = "%s@example.net" % author
+            author_email = "%s@%s" % (author, email_host)
             authors_fd.write(default_line % {'author': author,
                                              'author_name': author_name,
                                              'author_email': author_email})
+
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    #parser.add_option('input_file', metavar='FILENAME',
+    #                  help="The path to the input file")
+    parser.add_option('-o', metavar="FILENAME",
+                      help="The path to the output file",
+                      default="authors.txt", dest="output_file")
+    parser.add_option('--email-host', dest="email_host", default="example.net")
+    opts, args = parser.parse_args()
+
+    if len(args) != 1:
+        print "You must provide an input file"
+        sys.exit(0)
+    input_file = args[0]
+
+    # Add the input file here. Command line options to follow
+    authors = get_authors(input_file)
+    write_output(opts.output_file, authors, email_host=opts.email_host)
 
